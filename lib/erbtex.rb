b6356f63d38891bae7742cb4578d2c1ce4e1358a
Daniel E. Doherty <ded-law@ddoherty.net>
2013-01-14 16:17:29 -0600

    Get rid of former VERSION constant.  Now in erbtex/version.rb

--- lib/erbtex.rb
+++ lib/erbtex.rb
@@ -2,13 +2,9 @@
 
 require 'erubis'
 
+require 'erbtex/version'
 require 'erbtex/command_line'
 require 'erbtex/find_binary'
 require 'erbtex/runner'
 
-require 'ruby-debug'
-
-module ErbTeX
-  VERSION = ('$Release: 0.1.0 $' =~ /([.\d]+)/) && $1
-end
-
+require 'debugger'

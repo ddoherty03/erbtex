92216b2722be49d8fd4a2aea7ff0a936b5296a6f
Daniel E. Doherty <ded-law@ddoherty.net>
2017-02-11 15:18:02 -0600

    Test expand_input_file against file system.

--- spec/lib/command_line_spec.rb
+++ spec/lib/command_line_spec.rb
@@ -90,20 +90,52 @@ module ErbTeX
       end
     end
 
+    describe 'expand input file' do
       before :all do
+        unless Dir.exist?('tmp')
+          FileUtils.mkdir_p('tmp')
+        end
+      end
+
+      before :each do
+        FileUtils.rm_f Dir.glob('tmp/*')
       end
 
       after :all do
+        FileUtils.rm_rf('tmp')
       end
 
+      it 'expand file name with no extension and .tex existing' do
+        FileUtils.touch('tmp/junk.tex')
+        expect(CommandLine.expand_input_file('tmp/junk')).to eq('tmp/junk.tex')
       end
 
+      it 'expand file name with no extension and .tex.erb existing' do
+        FileUtils.touch('tmp/junk.tex.erb')
+        expect(CommandLine.expand_input_file('tmp/junk'))
+          .to eq('tmp/junk.tex.erb')
       end
 
+      it 'expand file name with no extension and .erb existing' do
+        FileUtils.touch('tmp/junk.erb')
+        expect(CommandLine.expand_input_file('tmp/junk'))
+          .to eq('tmp/junk.erb')
       end
 
+      it 'expand file name with .tex extension and existing' do
+        FileUtils.touch('tmp/junk.tex')
+        expect(CommandLine.expand_input_file('tmp/junk'))
+          .to eq('tmp/junk.tex')
       end
 
+      it 'expand file name with .tex extension and not existing' do
+        expect(CommandLine.expand_input_file('tmp/junk.tex'))
+          .to eq('tmp/junk.tex')
+      end
+
+      it 'expand file name with funny extension and not existing' do
+        expect(CommandLine.expand_input_file('tmp/junk.arb'))
+          .to eq('tmp/junk.arb')
       end
     end
   end
